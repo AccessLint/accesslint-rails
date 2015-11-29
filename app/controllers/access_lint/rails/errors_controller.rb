ea838458e12019ffe2bdd4090f610e2c372d8f16
Cameron Cundiff <github@ckundo.com>
2015-11-29 14:37:48 -0500

    Raise exception with the accessibility results
    - This error should be captures by the host applications exception handler.

--- app/controllers/access_lint/rails/errors_controller.rb
+++ app/controllers/access_lint/rails/errors_controller.rb
@@ -1,7 +1,11 @@
 module AccessLint
   module Rails
+    class Error < Exception; end
+
     class ErrorsController < AccessLint::Rails::ApplicationController
       def create
+        raise Error, params.fetch(:results)
+
         head :ok
       end
     end
