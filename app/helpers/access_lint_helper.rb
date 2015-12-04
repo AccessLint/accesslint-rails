cab7734edccd777516e04d8d15ebb45c10a9fbe5
Cameron Cundiff <github@ckundo.com>
2015-12-04 16:01:59 -0500

    Simplify javascript loading and execution
    - accesslint.js now handled the loading and execution.

--- app/helpers/access_lint_helper.rb
+++ app/helpers/access_lint_helper.rb
@@ -1,17 +1,5 @@
 module AccessLintHelper
   def include_access_lint
-    javascript_include_tag("accesslint") +
-    javascript_tag(<<-JS.strip_heredoc)
-      (function() {
-        var load = function() {
-          window.removeEventListener("load", load, false);
-          window.AccessLint.audit({
-            raiseError: #{!Rails.env.production?},
-          });
-        };
-
-        window.addEventListener("load", load);
-      })();
-    JS
+    javascript_include_tag("accesslint")
   end
 end
